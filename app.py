ba18e5e8e727a3ea57b1d188b01fb95181491a4a
MattCreigh <matthewCreigh99@gmail.com>
2018-02-25 15:58:24 +0000

    Fixed to where it was before i fucked up

--- app.py
+++ app.py
@@ -14,33 +14,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 
 def login():
-    return render_template
-    form = LogInForm(request.form)
-    if request.method == "POST":
-            log("UserName", form.UserName.data)
-            cur = mysql.connection.cursor()
-            cur.execute("""SELECT * FROM trunk_user_index WHERE userName = "%s";""" %form.UserName.data)
-            userIndex = cur.fetchone()
-            cur.close()
-            if sha256_crypt.verify(form.Password.data, (userIndex[2])):
-                return redirect(url_for("profile"))
-            else:
-                return redirect(url_for("login"))
-
-    return render_template("login.html", form=form)
-
-class LogInForm(Form):
-    UserName = StringField("UserName")
-    Password = PasswordField("Password")
-
-### ROUTE FOR PROFILE ########################################################################
-
-@app.route("/profile", methods=["GET","POST"])
-
-def profile():
-    return render_template("profile.html")
-
-
+    return render_template("login.html")
 
 ### SERVER INIT ###########################################################################
 
