36973ef88365b5f1d53f60624f28face2942bacf
MattCreigh <matthewCreigh99@gmail.com>
2018-02-25 15:40:42 +0000

    Step 2) Add login functionality

--- app.py
+++ app.py
@@ -1,21 +1,61 @@
 
 ### IMPORTS ################################################################################
 
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for, request
+from wtforms import Form, StringField, PasswordField
+from flask_mysqldb import MySQL
+from passlib.hash import sha256_crypt
+from testLib import *
 
 ### APP-CONFIG #############################################################################
 
 app = Flask(__name__)
+
+mysql = MySQL(app)
+
+### MySQL-CONFIG ############################################################################
+
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "H1nchley"
+app.config["MYSQL_DB"] = "schema2"
+app.config["MYSQL_HOST"] = "localhost"
+
+
+### SECRET KEY #############################################################################
+
+app.secret_key = "6d61374da4b4df53c6f8fbf4c9b05576d647a07da7498b400abaf7e1f4f44124"
 
 
 ### ROUTE FOR LOGIN ########################################################################
 
 @app.route("/", methods=["GET","POST"])
 
+def login():
+    form = LogInForm(request.form)
+    if request.method == "POST":
+            log("UserName", form.UserName.data)
+            cur = mysql.connection.cursor()
+            cur.execute("""SELECT * FROM trunk_user_index WHERE userName = "%s";""" %form.UserName.data)
+            userIndex = cur.fetchone()
+            cur.close()
+            if sha256_crypt.verify(form.Password.data, (userIndex[2])):
+                return redirect(url_for("profile"))
+            else:
+                return redirect(url_for("login"))
 
-def logIn():
-    return render_template("login.html")
+    return render_template("login.html", form=form)
+
+class LogInForm(Form):
+    UserName = StringField("UserName")
+    Password = PasswordField("Password")
+
+### ROUTE FOR PROFILE ########################################################################
+
+@app.route("/profile", methods=["GET","POST"])
+
+def profile():
+    return render_template("profile.html")
+
 
 
 ### SERVER INIT ###########################################################################
